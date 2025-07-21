d7e4420524ad35310b823046b19f1cf8c3f5e2d7
rajasekharreddy-git <mallidirajasekharreddy158@gmail.com>
2025-07-21 19:51:18 +0530

    Initial commit

---
+++ backend/config/connectToDB.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+ mongoose.connect(process.env.MONGO_URI);
+
+
+const connectToDB = () => {
+  mongoose
+    .connect(process.env.MONGO_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      
+      console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+      throw new Error(`Could not connect to MongoDB: ${err}`);
+    });
+};
+
+module.exports = connectToDB;
